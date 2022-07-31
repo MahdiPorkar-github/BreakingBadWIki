fc758949ee796aa05063e7130cf03b435266663d
Mahdi <MahdiPorkar2002@gmail.com>
2022-07-31 04:27:06 -0700

    viewBinding added to project and toolbar set as actionbar

--- app/src/main/java/com/example/breakingbadwiki/MainActivity.kt
+++ app/src/main/java/com/example/breakingbadwiki/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.breakingbadwiki
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.breakingbadwiki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarMain)
     }
 }
