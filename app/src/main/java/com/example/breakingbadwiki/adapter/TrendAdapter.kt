c484565ef7542c3624aa4b574404da3a323c0ed2
Mahdi <MahdiPorkar2002@gmail.com>
2022-08-01 07:25:02 -0700

    create and set adapter for trend recyclerView

---
+++ app/src/main/java/com/example/breakingbadwiki/adapter/TrendAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.breakingbadwiki.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.breakingbadwiki.data.ItemPost
+import com.example.breakingbadwiki.databinding.ItemTrendBinding
+
+class TrendAdapter(private val data: ArrayList<ItemPost>) :
+    RecyclerView.Adapter<TrendAdapter.TrendViewHolder>() {
+
+    lateinit var binding: ItemTrendBinding
+
+    inner class TrendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindViews(itemPost: ItemPost) {
+
+            Glide.with(itemView.context).load(itemPost.imageUrl).into(binding.itemTrendImgMain)
+            binding.itemTrendTxtTitle.text = itemPost.txtTitle
+            binding.itemTrendTxtSubtitle.text = itemPost.txtSubtitle
+            binding.itemTrendTxtInsight.text = itemPost.insight
+            binding.itemTrendCounter.text = (adapterPosition + 1).toString()
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrendViewHolder {
+
+        binding = ItemTrendBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TrendViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: TrendViewHolder, position: Int) {
+        holder.bindViews(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
